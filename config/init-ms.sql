0a71d27012c176065c0766bd1a350192150230bf
Kevin <Kevin.Xian.Lun.Lau@ibm.com>
2023-11-22 14:05:38 +0800

    patch mssql

---
+++ config/init-ms.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE [dockerdb]
+GO
+
+USE [dockerdb];
+GO
+
+CREATE TABLE Book (
+    id INT NOT NULL IDENTITY,
+    title TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+GO
+
+INSERT INTO [product] (title)
+VALUES 
+('Book A'),
+('Book B'),
+('Book C'),
+('Book D');
+GO
--- makefile
+++ makefile
@@ -1,3 +1,5 @@
 start:
 	docker-compose up -d && ./mvnw spring-boot:run -Dspring-boot.run.profiles=local 
 	
+up:
+	docker-compose up -d
